4cf8890a730bb87b58813607181fe05012f975cb
Ben Quigley <benjamin.s.quigley@gmail.com>
2018-09-12 15:53:44 -0400

    comments, slight style refactor

--- manage.py
+++ manage.py
@@ -3,13 +3,32 @@ import os
 
 from slugify import slugify
 
-def read_data(fn):
+TARGET_DIR = 'startbootstrap-simple-sidebar'
+CSV_SOURCE = 'Source.csv'
+HTML_TEMPLATE = os.path.join(TARGET_DIR, 'template.html')
+HTML_OUTFILE = os.path.join(TARGET_DIR, 'index.html')
+
+def read_csv_data(fn):
+	"""
+	Read data from the CSV source file. 
+	Ignore headers.
+	:return: an iterable.
+	"""
 	with open(fn, 'r', encoding="UTF-8") as infile:
 		reader = csv.reader(infile)
 		headers = next(reader)
 		for line in reader:
 			yield line
 
+	
+def get_html_template(fn):
+	"""
+	Read HTML data from the template source file.
+	:return: a string.
+	"""
+	with open(fn, 'r') as infile:
+		return ''.join((line for line in infile))
+			
 def clean(string):
 	"""
 	Removes double and trailing spaces.
@@ -17,8 +36,14 @@ def clean(string):
 	return string.strip().replace('  ', ' ')
 	
 class Store:
+	"""
+	Repository of RFP response information consisting of 
+	sections with questions and answers.
+	
+	write_html is used to create an html outfile in the 
+	"""
 	def __init__(self, filename):
-		data = read_data(filename)
+		data = read_csv_data(filename)
 		self.sections = {}
 		i = 0
 		for line in data:
@@ -30,7 +55,7 @@ class Store:
 		print("RFP Store data created successfully from {}.".format(filename))
 		print(i, "responses recorded.")
 	
-	def barf_html(self):
+	def _barf_html(self):
 		indentation = '                '
 		output = ""
 		for section, contents in self.sections.items():
@@ -45,7 +70,7 @@ class Store:
 			output += "{}</table>".format(indentation)
 		return output
 		
-	def barf_html_sections(self):
+	def _barf_html_sections(self):
 		indentation = '					'
 		output = ""
 		list_template = '{ind}<li>\n'\
@@ -56,25 +81,16 @@ class Store:
 			output += html_section
 		return output
 	
-	def get_html_template(self, fn):
-		with open(fn, 'r') as infile:
-			return "".join((line for line in infile))
-	
 	def write_html(self, source_file=None, outfile=None):
 		# TODO Honor indentation
-		if source_file is None:
-			source_file = os.path.join("startbootstrap-simple-sidebar", "index.html")
-		if outfile is None:
-			outfile = os.path.join('startbootstrap-simple-sidebar', 'outfile.html')
-		html = self.get_html_template(source_file)
-		html = html.replace("{ HEADERS }", self.barf_html_sections())
-		html = html.replace("{ CONTENT }", self.barf_html())
-		with open(outfile, 'w') as outfile:
+		html = get_html_template(HTML_TEMPLATE)
+		html = html.replace("{ HEADERS }", self._barf_html_sections())
+		html = html.replace("{ CONTENT }", self._barf_html())
+		with open(HTML_OUTFILE, 'w') as outfile:
 			outfile.write(html)
 
-		
 def main():
-	s = Store("Source.csv")
+	s = Store(CSV_SOURCE)
 	s.write_html()
 	
 if __name__ == "__main__":
