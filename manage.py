220b12e5e03a146e4267c7a41ab27b3ee5e376e7
Ben Quigley <benjamin.s.quigley@gmail.com>
2018-09-13 09:45:51 -0400

    changed output directory

--- manage.py
+++ manage.py
@@ -3,16 +3,16 @@ import os
 
 from slugify import slugify
 
-TARGET_DIR = 'startbootstrap-simple-sidebar'
+TARGET_DIR = 'output'
 CSV_SOURCE = 'Source.csv'
 HTML_TEMPLATE = os.path.join(TARGET_DIR, 'template.html')
 HTML_OUTFILE = os.path.join(TARGET_DIR, 'index.html')
 
 def read_csv_data(fn):
 	"""
-	Read data from the CSV source file. 
+	Read data from the CSV source file.
 	Ignore headers.
-	:return: an iterable.
+	:return: an iterable of rows of strings.
 	"""
 	with open(fn, 'r', encoding="UTF-8") as infile:
 		reader = csv.reader(infile)
@@ -20,7 +20,7 @@ def read_csv_data(fn):
 		for line in reader:
 			yield line
 
-	
+
 def get_html_template(fn):
 	"""
 	Read HTML data from the template source file.
@@ -28,19 +28,30 @@ def get_html_template(fn):
 	"""
 	with open(fn, 'r') as infile:
 		return ''.join((line for line in infile))
-			
-def clean(string):
+
+def post_process(string):
 	"""
-	Removes double and trailing spaces.
+	Remove double and trailing spaces.
+	Replace codes with words.
 	"""
-	return string.strip().replace('  ', ' ')
-	
+	string = string.strip()
+	string = string.replace('  ', ' ')
+	codes = {"Y": "Existing functionality",
+				"F": "On the roadmap",
+				"C": "Available by customization",
+				"V": "Available via a vendor",
+				"T": "Available via a third party",
+			}
+	if len(string) == 1 and string in codes.keys():
+		string = "{} - {}".format(string, codes[string])
+	return string
+
 class Store:
 	"""
-	Repository of RFP response information consisting of 
+	Repository of RFP response information consisting of
 	sections with questions and answers.
-	
-	write_html is used to create an html outfile in the 
+
+	write_html is used to create an html outfile in the target directory.
 	"""
 	def __init__(self, filename):
 		data = read_csv_data(filename)
@@ -54,22 +65,36 @@ class Store:
 			self.sections[header].append(line[1:3])
 		print("RFP Store data created successfully from {}.".format(filename))
 		print(i, "responses recorded.")
-	
+
 	def _barf_html(self):
 		indentation = '                '
 		output = ""
 		for section, contents in self.sections.items():
+			# Section header:
 			output += '{}<h1 id="{}">{}</h1>\n'.format(indentation, slugify(section), section)
+			# Table of questions & answers:
 			output += '{}<table>'.format(indentation)
 			for question, answer in contents:
+				question = post_process(question)
 				question_html ="<p>{}</p>\n".format(question)
 				answer_html = ""
+				list_mode = False
 				for answer_line in answer.split("\n"):
-					answer_html += "<p>{}</p>\n".format(clean(answer_line))
+					answer_line = post_process(answer_line)
+					if answer_line[:2] == "* ":
+						if list_mode == False:
+							list_mode = True
+							answer_html += "{}<ul>\n".format(indentation)
+						answer_html += "{}    <li>{}</li>\n".format(indentation, answer_line[2:])
+					else:
+						if list_mode == True:
+							list_mode = False
+							answer_html += "{}</ul>\n".format(indentation)
+						answer_html += "<p>{}</p>\n".format(post_process(answer_line))
 				output += "{}<tr><td>{}</td><td>{}</td></tr>".format(indentation, question_html, answer_html)
 			output += "{}</table>".format(indentation)
 		return output
-		
+
 	def _barf_html_sections(self):
 		indentation = '					'
 		output = ""
@@ -80,7 +105,7 @@ class Store:
 			html_section = list_template.format(slugify(section), section, ind=indentation)
 			output += html_section
 		return output
-	
+
 	def write_html(self, source_file=None, outfile=None):
 		# TODO Honor indentation
 		html = get_html_template(HTML_TEMPLATE)
@@ -92,6 +117,6 @@ class Store:
 def main():
 	s = Store(CSV_SOURCE)
 	s.write_html()
-	
+
 if __name__ == "__main__":
 	main()
